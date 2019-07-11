dfd853f545eb6018043019006119ae3d30eee526
Elie Andraos <elie@andraos.org>
2019-07-11 14:20:31 +0300

    :sparkles: Add users table seeders

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'Level Up Admin',
+            'email' => 'admin@levelup.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
